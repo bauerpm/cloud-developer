2d9f429bc7ab4a26657fcedc2f2426fccefad03e
Mike Bauer <mikeb@parsed.io>
2021-01-31 14:58:07 -0500

    basic service excercises

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -15,3 +15,10 @@ export const divide = (a: number, b: number) => {
 // it should return one string combining the two strings.
 // it should throw an error if either of the strings are empty.
 // ensure your function is exported.
+
+export const concatStrings = (str1: string, str2: string) => {
+    if (!str1 || !str2) {
+        throw new Error('two non-empty strings are required')
+    }
+    return str1.concat(str2)
+}
